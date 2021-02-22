9fbf2be94d012b651335291d00e786251d1f553a
jinman <wmlsakdl@gmail.com>
2021-02-22 23:48:22 +0900

    샘플 프로젝트 초기셋팅

---
+++ common/src/main/java/api/CommonClass.java
@@ -0,0 +1,11 @@
+package api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CommonClass {
+
+    private String value;
+}
---
+++ web-api/src/main/java/api/TestController.java
@@ -0,0 +1,15 @@
+package api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public Mono<CommonClass> test() {
+
+        return Mono.just(new CommonClass("test!!"));
+    }
+}
